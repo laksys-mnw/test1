1c33613c812416c098c4dec01703781577ea734f
laksys linux <laksys@gmail.com>
2021-10-01 06:02:28 +0530

    thread was added

--- mnxwdemo/src/com/king/App.java
+++ mnxwdemo/src/com/king/App.java
@@ -3,5 +3,10 @@ package com.king;
 public class App {
 	public static void main(String[] args) {
 		System.out.println("hello world!");
+		
+		new Thread(()->{
+			for(int i=0; i<10; i++)
+				System.out.println("worker thread...");
+		}).start();
 	}
 }
